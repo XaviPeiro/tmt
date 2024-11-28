2b73b2c46c037b5f42c33e9451c8176f1362c54f
Xavier <xavier.peiro@fetch.ai>
2024-11-28 19:44:19 +0100

    test with db + rm unwanted change

--- interview/order/tests/conftest.py
+++ interview/order/tests/conftest.py
@@ -4,3 +4,9 @@ from rest_framework.test import APIClient
 @pytest.fixture
 def api_client():
     return APIClient()
+
+@pytest.fixture(scope='session', autouse=True)
+def setup_db(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        # Awful approach, I know :(
+        import database
--- interview/order/views.py
+++ interview/order/views.py
@@ -9,10 +9,7 @@ from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
 
 # Create your views here.
-class OrderListCreateView(
-    generics.ListCreateAPIView,
-    mixins.UpdateModelMixin,
-):
+class OrderListCreateView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
